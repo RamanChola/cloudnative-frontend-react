8ef36bceebac5d76a8db59db232aec048f1c3740
iota-008 <raiboleankit8@gmail.com>
2021-12-12 17:47:03 +0530

    ui change

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -19,63 +19,34 @@ import { react } from "./Questions/ReactQuiz";
 import { display, margin, textAlign } from "@mui/system";
 
 const Homepage = () => {
-  const history = useHistory();
-  const auth = React.useContext(AuthContext);
+	const history = useHistory();
+	const auth = React.useContext(AuthContext);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  Survey.StylesManager.applyTheme("bootstrap");
+	var defaultThemeColors = Survey.StylesManager.ThemeColors["default"];
+	defaultThemeColors["$main-color"] = "#7ff07f";
+	defaultThemeColors["$main-hover-color"] = "#6fe06f";
+	defaultThemeColors["$text-color"] = "#4a4a4a";
+	defaultThemeColors["$header-color"] = "#7ff07f";
 
-  var myCss = {
-    matrix: {
-      root: "table table-striped",
-    },
-    navigationButton: "button btn-lg",
-  };
+	defaultThemeColors["$header-background-color"] = "#4a4a4a";
+	defaultThemeColors["$body-container-background-color"] = "#f8f8f8";
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "20px",
-      }}
-    >
-      <Navbar />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "auto",
-          margin: "10px 20px",
-        }}
-      >
-        <div style={{ textAlign: "center", margin: "10px" }}>
-          Welcome To the Homepage
-        </div>
-        <Survey.Survey json={questions} css={myCss} />
-      </div>
-      <div className="above-alan">
-        Click the button below and say What can I do here ?
-      </div>
-    </div>
-  );
-=======
+	var myCss = {
+		matrix: {
+			root: "table my-custom-style",
+		},
+		navigationButton: "button btn-lg",
+	};
+
+	Survey.StylesManager.ThemeCss[".sv_default_css .my-custom-style"] =
+		"background-color: $main-color; border: 5px solid $main-hover-color;";
+
+	Survey.StylesManager.applyTheme();
+
 	const [quizType, setQuizType] = useState(html);
-=======
-  const [quizType, setQuizType] = useState(html);
->>>>>>> upstream/main
 
-  Survey.StylesManager.applyTheme("bootstrap Material");
+	// Survey.StylesManager.applyTheme("bootstrap Material");
 
-  var myCss = {
-    matrix: {
-      root: "table table-striped",
-    },
-    navigationButton: "button btn-sm",
-  };
-
-<<<<<<< HEAD
 	return (
 		<div
 			style={{
@@ -92,135 +63,97 @@ const Homepage = () => {
 					margin: "10px",
 				}}
 			>
-				<h1>Welcome To the Homepage</h1>
+				{/* <h1>Welcome To the Homepage</h1> */}
 				<div
 					style={{
 						display: "flex",
-						justifyContent: "space-between",
+						justifyContent: "center",
 						textAlign: "center",
-						backgroundColor: "black",
+						color: "black",
+						gap: "10px",
 					}}
 				>
-					<button onClick={() => setQuizType(html)}>html</button>
-					<button onClick={() => setQuizType(css)}>css</button>
-					<button onClick={() => setQuizType(javascript)}>javascript</button>
-					<button onClick={() => setQuizType(mongodb)}>mongodb</button>
-					<button onClick={() => setQuizType(node)}>node</button>
-					<button onClick={() => setQuizType(react)}>react</button>
+					<button
+						style={{
+							color: "black",
+							border: "1px solid grey",
+							padding: "10px",
+							boxShadow: " 0 0 0 1px grey",
+							borderRadius: "5%",
+						}}
+						onClick={() => setQuizType(html)}
+					>
+						html
+					</button>
+					<button
+						style={{
+							color: "black",
+							border: "1px solid grey",
+							padding: "10px",
+							boxShadow: " 0 0 0 1px grey",
+							borderRadius: "5%",
+						}}
+						onClick={() => setQuizType(css)}
+					>
+						css
+					</button>
+					<button
+						style={{
+							color: "black",
+							border: "1px solid grey",
+							padding: "10px",
+							boxShadow: " 0 0 0 1px grey",
+							borderRadius: "5%",
+						}}
+						onClick={() => setQuizType(javascript)}
+					>
+						javascript
+					</button>
+					<button
+						style={{
+							color: "black",
+							border: "1px solid grey",
+							padding: "10px",
+							boxShadow: " 0 0 0 1px grey",
+							borderRadius: "5%",
+						}}
+						onClick={() => setQuizType(mongodb)}
+					>
+						mongodb
+					</button>
+					<button
+						style={{
+							color: "black",
+							border: "1px solid grey",
+							padding: "10px",
+							boxShadow: " 0 0 0 1px grey",
+							borderRadius: "5%",
+						}}
+						onClick={() => setQuizType(node)}
+					>
+						node
+					</button>
+					<button
+						style={{
+							color: "black",
+							border: "1px solid grey",
+							padding: "10px",
+							boxShadow: " 0 0 0 1px grey",
+							borderRadius: "5%",
+						}}
+						onClick={() => setQuizType(react)}
+					>
+						react
+					</button>
 				</div>
 			</div>
-			<div className='main'>
+			<div className='container'>
 				<Survey.Survey json={quizType} css={myCss} />
+				{/* <div className=''> */}
+				{/* Click the button below and say What can I do here ?</div> */}
 			</div>
 		</div>
 	);
->>>>>>> edb7587b01b514d88ed02fe5309ca337cb5c575c
-=======
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        margin: "20px 10px",
-        gap: "20px",
-      }}
-    >
-      <Navbar />
-      <div
-        style={{
-          textAlign: "center",
-          margin: "10px",
-        }}
-      >
-        <h1>Welcome To the Homepage</h1>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            textAlign: "center",
-            color: "black",
-          }}
-        >
-          <button
-            style={{
-              color: "black",
-              border: "1px solid grey",
-              padding: "15px",
-              boxShadow: " 0 0 0 1px grey",
-              borderRadius: "5%",
-            }}
-            onClick={() => setQuizType(html)}
-          >
-            html
-          </button>
-          <button
-            style={{
-              color: "black",
-              border: "1px solid grey",
-              padding: "15px",
-              boxShadow: " 0 0 0 1px grey",
-              borderRadius: "5%",
-            }}
-            onClick={() => setQuizType(css)}
-          >
-            css
-          </button>
-          <button
-            style={{
-              color: "black",
-              border: "1px solid grey",
-              padding: "15px",
-              boxShadow: " 0 0 0 1px grey",
-              borderRadius: "5%",
-            }}
-            onClick={() => setQuizType(javascript)}
-          >
-            javascript
-          </button>
-          <button
-            style={{
-              color: "black",
-              border: "1px solid grey",
-              padding: "15px",
-              boxShadow: " 0 0 0 1px grey",
-              borderRadius: "5%",
-            }}
-            onClick={() => setQuizType(mongodb)}
-          >
-            mongodb
-          </button>
-          <button
-            style={{
-              color: "black",
-              border: "1px solid grey",
-              padding: "15px",
-              boxShadow: " 0 0 0 1px grey",
-              borderRadius: "5%",
-            }}
-            onClick={() => setQuizType(node)}
-          >
-            node
-          </button>
-          <button
-            style={{
-              color: "black",
-              border: "1px solid grey",
-              padding: "15px",
-              boxShadow: " 0 0 0 1px grey",
-              borderRadius: "5%",
-            }}
-            onClick={() => setQuizType(react)}
-          >
-            react
-          </button>
-        </div>
-      </div>
-      <div className="main">
-        <Survey.Survey json={quizType} css={myCss} />
-      </div>
-    </div>
-  );
->>>>>>> upstream/main
 };
 
 export default Homepage;
--- src/pages/Questions/JavaScriptQuiz.js
+++ src/pages/Questions/JavaScriptQuiz.js
@@ -1,135 +1,134 @@
 export const javascript = {
-  title: "Javascript Quiz",
+	title: "Javascript Quiz",
 
-  completeText: "Finish",
-  pageNextText: "Continue",
-  pagePrevText: "Previous",
-  showProgressBar: "top",
-  progressBarType: "buttons",
-  showTimerPanel: "top",
-  maxTimeToFinishPage: 30,
-  maxTimeToFinish: 180,
-  firstPageIsStarted: true,
-  startSurveyText: "Start Quiz",
-  pages: [
-    {
-      navigationTitle: "Start Quiz",
+	completeText: "Finish",
+	pageNextText: "Continue",
+	pagePrevText: "Previous",
+	showProgressBar: "top",
+	progressBarType: "buttons",
+	showTimerPanel: "top",
+	maxTimeToFinishPage: 30,
+	maxTimeToFinish: 180,
+	firstPageIsStarted: true,
+	startSurveyText: "Start Quiz",
+	pages: [
+		{
+			navigationTitle: "Start Quiz",
 
-      elements: [
-        {
-          type: "panel",
-          elements: [
-            {
-              type: "javascript",
-              name: "quiz_intro",
-              html:
-                "<article class='intro'>    <h1 class='intro__heading intro__heading--income title'>                     javascript Development Quiz              </h1>    <div class='intro__body wysiwyg'>       <p> You are about to start quiz which contains questions related to web development. </p>   <p>You have 30 seconds for every page and 180 seconds for the whole survey of 6 questions.</p>  <p>Please click on <b>'Start Quiz'</b> button when you are ready.</p>  </div> </article>",
-            },
-          ],
-          name: "panel1",
-        },
-      ],
-    },
-    {
-      navigationTitle: "Question 1",
-      questions: [
-        {
-          type: "radiogroup",
-          // name: "HTML fullform",
-          title:
-            "Which of the following statements defines JavaScript correctly?",
-          choices: [
-            "JavaScript is a scripting language used to make the website interactive",
-            "JavaScript is an assembly language used to make the website interactive",
-            "JavaScript is a compiled language used to make the website interactive",
-            "None of the above",
-          ],
-          correctAnswer:
-            "JavaScript is an assembly language used to make the website interactive",
-        },
-      ],
-    },
-    {
-      navigationTitle: "Question 2",
-      questions: [
-        {
-          type: "radiogroup",
-          // name: "keywords",
-          title:
-            "What will be the output of the following JavaScript program: <p id='demo'></p> <script>var js = 10;js *= 5;document.getElementById('demo').innerHTML = js;</script> ",
-          choicesOrder: "random",
-          choices: ["10", "50", "5", "Error"],
-          correctAnswer: "50",
-        },
-      ],
-    },
-    {
-      navigationTitle: "Question 3",
-      questions: [
-        {
-          type: "radiogroup",
-          // name: "magnacarta",
-          title:
-            "Will the following JavaScript code work: var js = (function(x) {return x*x;}(10));",
-          choicesOrder: "random",
-          choices: [
-            "Exception will be thrown",
-            "Memory leak",
-            "Error",
-            "Yes, perfectly",
-          ],
-          correctAnswer: "Yes, perfectly",
-        },
-      ],
-    },
-    {
-      navigationTitle: "Question 4",
-      questions: [
-        {
-          type: "radiogroup",
-          // name: "magnacarta",
-          title:
-            "Which of the following can be used to call a JavaScript Code Snippet?",
-          choicesOrder: "random",
-          choices: [
-            "Function/Method",
-            "Preprocessor",
-            "Triggering Event",
-            "RMI",
-          ],
-          correctAnswer: "Function/Method",
-        },
-      ],
-    },
-    {
-      navigationTitle: "Question 5",
-      questions: [
-        {
-          type: "radiogroup",
-          // name: "magnacarta",
-          title:
-            "What will be the output of the following JavaScript function: <p id='demo'></p><script>function javascript(){// javacript abs() methoddocument.getElementById('demo').innerHTML = Math.abs(-7.25);}</script>",
-          choicesOrder: "random",
-          choices: ["-7.25", "7.25", "-7", "7"],
-          correctAnswer: "7.25",
-        },
-      ],
-    },
-    {
-      navigationTitle: "Question 6",
-      questions: [
-        {
-          type: "radiogroup",
-          // name: "magnacarta",
-          title:
-            "What will be the output of the following JavaScript code snippet: int a=4;int b=1;int c=0;// JavaScript if statement and equalto operator; If(a==b){document.write(a)};else if(a==c){document.write(a)};else{document.write(c);}",
-          choicesOrder: "random",
-          choices: ["0", "1", "4", "Error"],
-          correctAnswer: "0",
-        },
-      ],
-    },
-  ],
-  completedHtml:
-    "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>",
+			elements: [
+				{
+					type: "panel",
+					elements: [
+						{
+							type: "html",
+							name: "quiz_intro",
+							html: "<article class='intro'>    <h1 class='intro__heading intro__heading--income title'>                     javascript Development Quiz              </h1>    <div class='intro__body wysiwyg'>       <p> You are about to start quiz which contains questions related to web development. </p>   <p>You have 30 seconds for every page and 180 seconds for the whole survey of 6 questions.</p>  <p>Please click on <b>'Start Quiz'</b> button when you are ready.</p>  </div> </article>",
+						},
+					],
+					name: "panel1",
+				},
+			],
+		},
+		{
+			navigationTitle: "Question 1",
+			questions: [
+				{
+					type: "radiogroup",
+					// name: "HTML fullform",
+					title:
+						"Which of the following statements defines JavaScript correctly?",
+					choices: [
+						"JavaScript is a scripting language used to make the website interactive",
+						"JavaScript is an assembly language used to make the website interactive",
+						"JavaScript is a compiled language used to make the website interactive",
+						"None of the above",
+					],
+					correctAnswer:
+						"JavaScript is an assembly language used to make the website interactive",
+				},
+			],
+		},
+		{
+			navigationTitle: "Question 2",
+			questions: [
+				{
+					type: "radiogroup",
+					// name: "keywords",
+					title:
+						"What will be the output of the following JavaScript program: <p id='demo'></p> <script>var js = 10;js *= 5;document.getElementById('demo').innerHTML = js;</script> ",
+					choicesOrder: "random",
+					choices: ["10", "50", "5", "Error"],
+					correctAnswer: "50",
+				},
+			],
+		},
+		{
+			navigationTitle: "Question 3",
+			questions: [
+				{
+					type: "radiogroup",
+					// name: "magnacarta",
+					title:
+						"Will the following JavaScript code work: var js = (function(x) {return x*x;}(10));",
+					choicesOrder: "random",
+					choices: [
+						"Exception will be thrown",
+						"Memory leak",
+						"Error",
+						"Yes, perfectly",
+					],
+					correctAnswer: "Yes, perfectly",
+				},
+			],
+		},
+		{
+			navigationTitle: "Question 4",
+			questions: [
+				{
+					type: "radiogroup",
+					// name: "magnacarta",
+					title:
+						"Which of the following can be used to call a JavaScript Code Snippet?",
+					choicesOrder: "random",
+					choices: [
+						"Function/Method",
+						"Preprocessor",
+						"Triggering Event",
+						"RMI",
+					],
+					correctAnswer: "Function/Method",
+				},
+			],
+		},
+		{
+			navigationTitle: "Question 5",
+			questions: [
+				{
+					type: "radiogroup",
+					// name: "magnacarta",
+					title:
+						"What will be the output of the following JavaScript function: <p id='demo'></p><script>function javascript(){// javacript abs() methoddocument.getElementById('demo').innerHTML = Math.abs(-7.25);}</script>",
+					choicesOrder: "random",
+					choices: ["-7.25", "7.25", "-7", "7"],
+					correctAnswer: "7.25",
+				},
+			],
+		},
+		{
+			navigationTitle: "Question 6",
+			questions: [
+				{
+					type: "radiogroup",
+					// name: "magnacarta",
+					title:
+						"What will be the output of the following JavaScript code snippet: int a=4;int b=1;int c=0;// JavaScript if statement and equalto operator; If(a==b){document.write(a)};else if(a==c){document.write(a)};else{document.write(c);}",
+					choicesOrder: "random",
+					choices: ["0", "1", "4", "Error"],
+					correctAnswer: "0",
+				},
+			],
+		},
+	],
+	completedHtml:
+		"<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>",
 };
