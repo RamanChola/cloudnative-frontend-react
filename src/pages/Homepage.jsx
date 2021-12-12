ee4a6f826caccfaff657d61d8d39da48e6d891d4
RamanChola <ramanchola3@gmail.com>
2021-12-12 16:52:50 +0530

    improved homepage styling

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -19,58 +19,124 @@ import { react } from "./Questions/ReactQuiz";
 import { display, margin, textAlign } from "@mui/system";
 
 const Homepage = () => {
-	const history = useHistory();
-	const auth = React.useContext(AuthContext);
+  const history = useHistory();
+  const auth = React.useContext(AuthContext);
 
-	const [quizType, setQuizType] = useState(html);
+  const [quizType, setQuizType] = useState(html);
 
-	Survey.StylesManager.applyTheme("bootstrap");
+  Survey.StylesManager.applyTheme("bootstrap Material");
 
-	var myCss = {
-		matrix: {
-			root: "table table-striped",
-		},
-		navigationButton: "button btn-sm",
-	};
+  var myCss = {
+    matrix: {
+      root: "table table-striped",
+    },
+    navigationButton: "button btn-sm",
+  };
 
-	return (
-		<div
-			style={{
-				display: "flex",
-				flexDirection: "column",
-				margin: "20px 10px",
-				gap: "20px",
-			}}
-		>
-			<Navbar />
-			<div
-				style={{
-					textAlign: "center",
-					margin: "10px",
-				}}
-			>
-				<h1>Welcome To the Homepage</h1>
-				<div
-					style={{
-						display: "flex",
-						justifyContent: "space-between",
-						textAlign: "center",
-						backgroundColor: "black",
-					}}
-				>
-					<button onClick={() => setQuizType(html)}>html</button>
-					<button onClick={() => setQuizType(css)}>css</button>
-					<button onClick={() => setQuizType(javascript)}>javascript</button>
-					<button onClick={() => setQuizType(mongodb)}>mongodb</button>
-					<button onClick={() => setQuizType(node)}>node</button>
-					<button onClick={() => setQuizType(react)}>react</button>
-				</div>
-			</div>
-			<div className='main'>
-				<Survey.Survey json={quizType} css={myCss} />
-			</div>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "20px 10px",
+        gap: "20px",
+      }}
+    >
+      <Navbar />
+      <div
+        style={{
+          textAlign: "center",
+          margin: "10px",
+        }}
+      >
+        <h1>Welcome To the Homepage</h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            textAlign: "center",
+            color: "black",
+          }}
+        >
+          <button
+            style={{
+              color: "black",
+              border: "1px solid grey",
+              padding: "15px",
+              boxShadow: " 0 0 0 1px grey",
+              borderRadius: "5%",
+            }}
+            onClick={() => setQuizType(html)}
+          >
+            html
+          </button>
+          <button
+            style={{
+              color: "black",
+              border: "1px solid grey",
+              padding: "15px",
+              boxShadow: " 0 0 0 1px grey",
+              borderRadius: "5%",
+            }}
+            onClick={() => setQuizType(css)}
+          >
+            css
+          </button>
+          <button
+            style={{
+              color: "black",
+              border: "1px solid grey",
+              padding: "15px",
+              boxShadow: " 0 0 0 1px grey",
+              borderRadius: "5%",
+            }}
+            onClick={() => setQuizType(javascript)}
+          >
+            javascript
+          </button>
+          <button
+            style={{
+              color: "black",
+              border: "1px solid grey",
+              padding: "15px",
+              boxShadow: " 0 0 0 1px grey",
+              borderRadius: "5%",
+            }}
+            onClick={() => setQuizType(mongodb)}
+          >
+            mongodb
+          </button>
+          <button
+            style={{
+              color: "black",
+              border: "1px solid grey",
+              padding: "15px",
+              boxShadow: " 0 0 0 1px grey",
+              borderRadius: "5%",
+            }}
+            onClick={() => setQuizType(node)}
+          >
+            node
+          </button>
+          <button
+            style={{
+              color: "black",
+              border: "1px solid grey",
+              padding: "15px",
+              boxShadow: " 0 0 0 1px grey",
+              borderRadius: "5%",
+            }}
+            onClick={() => setQuizType(react)}
+          >
+            react
+          </button>
+        </div>
+      </div>
+      <div className="main">
+        <Survey.Survey json={quizType} css={myCss} />
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
